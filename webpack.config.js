48e6be06d04789597cc16b09c86ab975257f49cb
Kostya20292 <78802135+Kostya20292@users.noreply.github.com>
2022-09-20 23:13:31 +0300

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,24 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        main: path.resolve(__dirname, './src/index.js'),
+        main: ['@babel/polyfill', path.resolve(__dirname, './src/index.js')],
     },
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/i,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
     },
 };
